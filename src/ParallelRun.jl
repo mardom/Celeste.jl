dcddf0e123d1d772c799db2b6147f6456c1fc3e8
Kiran Pamnany <kiran.pamnany@intel.com>
2016-09-15 10:55:18 -0700

    Updating to use serializing Garbo, debugging

--- src/ParallelRun.jl
+++ src/ParallelRun.jl
@@ -96,8 +96,8 @@ function divide_skyarea(box, nra, ndec, i)
 end
 
 
-@inline nputs(nid, s) = ccall(:puts, Cint, (Cstring,), string("[$nid] ", s))
-@inline ntputs(nid, tid, s) = ccall(:puts, Cint, (Cstring,), string("[$nid]<$tid> ", s))
+@inline nputs(nid, s...) = ccall(:puts, Cint, (Cstring,), string("[$nid] ", s...))
+@inline ntputs(nid, tid, s...) = ccall(:puts, Cint, (Cstring,), string("[$nid]<$tid> ", s...))
 @inline phalse(b) = b[] = false
 
 
